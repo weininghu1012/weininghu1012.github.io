27991592fe0b1bd98d585e56a68ce0956a95a59a
Weining Hu <weininghu1012@gmail.com>
2015-08-07 18:15:22 -0700

    add bubble chart

--- R/employment.R
+++ R/employment.R
@@ -1,7 +1,41 @@
-employ = read.csv(file = "/Users/weininghu/Desktop/employment1.csv", header = T, sep = ',')
 install.packages("googleVis")
 library(googleVis)
+employ = read.csv(file = "/Users/weininghu/Desktop/employment1.csv", header = T, sep = ',')
+
 Motion=gvisMotionChart(employ,idvar="Gender",timevar="Year")
 plot(Motion)
 Motion$html$chart
 cat(Motion$html$chart, file = "/Users/weininghu/Desktop/wis.html")
+
+
+fe = read.csv(file = "/Users/weininghu/Desktop/weininghu1012.github.io/R/female_engineer.csv")
+head(fe)
+# The order of the company, bubble color change according to the company
+# ALL
+# Dropbox
+# Yelp
+# Pinterest
+# PayPal(Braintree)
+# Etsy
+# Airbnb
+# GitHub
+# Foursquare
+# Eventbrite
+# Flipboard
+# Yahoo(Flicker)
+# HootSuite
+# Stack Exchange
+# Khan Academy
+# Quora
+# Coursera
+# Facebook(Parse)
+# Snapchat
+# Codecademy
+# The New York Times(New Products)
+
+
+Bubble = gvisBubbleChart(fe, idvar = "company",xvar = "num_female_eng",yvar = "percent_female_eng",sizevar = "num_eng",
+                         options=list(colors="['#888888',#2895F1','#c41200','#C92228','#0079ad','#f45800','#fd5c63','#333333','#f94877',
+                                      '#F3844C','#e12828','#400191','#71685f','#366fb3','#9cb443','#a82400','#3A6D8E','#3b5998',
+                                     '#fffc00','#204056','#000']"))
+plot(Bubble)
